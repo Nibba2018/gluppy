64cc74981eaa0947a3df1f945227563211b8cb0f
Nibba2018 <sohambiswas41@gmail.com>
2020-09-03 20:47:16 +0530

    update plotting main

--- plotting.py
+++ plotting.py
@@ -4,7 +4,7 @@ import OpenGL.GL as gl
 from sklearn.linear_model import LinearRegression
 
 def regPlot(x_points, y_points, line_color=(1, 0, 0), point_color=(0, 0, 1),
-            axes_color=(0, 1, 0), final_x=500):
+            axes_color=(0, 1, 0), final_x=500, plot_points=False):
 
     # Draw axes
     gl.glColor3f(*axes_color)
@@ -23,22 +23,39 @@ def regPlot(x_points, y_points, line_color=(1, 0, 0), point_color=(0, 0, 1),
     gl.glColor3f(*line_color)
     drawDDA((0, initial_y), (final_x, final_y))
 
-    #Draw Points
-    gl.glColor3f(*point_color)
-    for x, y in zip(x_points, y_points):
-        drawDot(x, y)
+    if plot_points:
+        #Draw Points
+        gl.glColor3f(*point_color)
+        for x, y in zip(x_points, y_points):
+            drawDot(x, y)
 
 
 if __name__ == "__main__":
     import numpy as np
     from window import init_window
+    import OpenGL.GLUT as glut
+
+    point_color=(0, 0, 1)
+    x_points = np.random.rand(50) * 500
+    y_points = 200 + np.random.rand(50) * (500 - 200)
+
+    clicked = False
+
+    def mouseClicked(button, button_state, cursor_x, cursor_y):
+        if (button == glut.GLUT_LEFT_BUTTON and
+            button_state == glut.GLUT_DOWN):
+            global clicked
+            if not clicked:
+                #Draw Points
+                gl.glColor3f(*point_color)
+                for x, y in zip(x_points.astype(int), y_points.astype(int)):
+                    drawDot(x, y)
+                clicked = True
+                gl.glFlush()
 
     def display_plot():
-        x_points = np.random.rand(50) * 500
-        y_points = 200 + np.random.rand(50) * (500 - 200)
-
         regPlot(x_points.astype(int), y_points.astype(int))
         gl.glFlush()
 
     init_window(display_plot, title="Regression Plot", point_size=4.0,
-                bg_color=(1, 1, 0, 1))
+                bg_color=(1, 1, 0, 1), mouse_callback=mouseClicked)
